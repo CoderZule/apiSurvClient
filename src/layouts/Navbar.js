a96a5db084f631b48ab800daf0f9146b6fbd1f0f
Mariem Derbali <mariem.derbali20@gmail.com>
2024-11-08 19:51:02 +0100

    real time changes : fist & last name fixes

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,17 +1,49 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { logoutUser } from '../actions/userActions';
+import { logoutUser, getUserById } from '../actions/userActions';
 import { Redirect, Link } from 'react-router-dom';
+import io from 'socket.io-client';
 
 export default function Navbar() {
-    const { currentUser } = useSelector((state) => state.loginUserReducer);
+    const currentUser = useSelector(state => state.loginUserReducer.currentUser);
+
     const dispatch = useDispatch();
+
+    const [Firstname, setFirstname] = useState('');
+    const [Lastname, setLastname] = useState('');
+    const getUserByIdState = useSelector(state => state.getUserByIdReducer);
+    const { user } = getUserByIdState;
+
+    useEffect(() => {
+        if (currentUser) {
+            const { _id } = currentUser;
+            dispatch(getUserById(_id));
+        }
+    }, [dispatch, currentUser]);
 
     const handleLogout = () => {
         dispatch(logoutUser());
     };
 
-   
+    useEffect(() => {
+        const socket = io('http://localhost:3000');
+
+        return () => {
+            socket.disconnect();
+        };
+    }, []);
+
+    useEffect(() => {
+        if (user) {
+            const { Firstname, Lastname } = user;
+            setFirstname(Firstname);
+            setLastname(Lastname);
+        }
+    }, [user]);
+
+
+
+
     if (!currentUser) {
         // Redirect to login page if user is not authenticated
         return <Redirect to="/" />;
@@ -24,7 +56,7 @@ export default function Navbar() {
                 <li className="nav-item dropdown no-arrow">
                     <a className="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <span className="mr-2  d-lg-inline text-gray-600 small">{currentUser.Firstname} {currentUser.Lastname}</span>
+                        <span className="mr-2  d-lg-inline text-gray-600 small">{Firstname} {Lastname}</span>
                         <img className="img-profile rounded-circle" src="/img/undraw_profile.svg" alt="Profile" />
                     </a>
                     <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in"
